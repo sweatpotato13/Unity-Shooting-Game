693342478892bbbb582cbcf09e0fa50a0367cd8f
JangChorong <sweatpotato13@gmail.com>
2020-05-02 20:46:24 +0900

    Make Scene Change for option & shop

---
+++ Assets/Scripts/MainScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeOptionScene()
+    {
+        SceneManager.LoadScene("OptionScreen");
+    }
+
+    public void ChangeShopScene()
+    {
+        SceneManager.LoadScene("ShopScreen");
+    }
+}
