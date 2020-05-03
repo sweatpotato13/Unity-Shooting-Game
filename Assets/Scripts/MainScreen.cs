f1c21f3f2d864e0316436456836ca95b9c2b01ad
leivoev <leivoev@gmail.com>
2020-05-04 00:40:38 +0900

    Make StageSelectScene and Now Start Button works

---
+++ Assets/Scripts/MainScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeStageSelectScene()
+    {
+      SceneManager.LoadScene("StageSelectScene");
+    }
+}
