34ed59f8467df1a4deb041bb9c0647b7e7fd4fdb
Jang Chorong <sweatpotato13@gmail.com>
2020-05-04 13:21:25 +0900

    Make BGM & Implemented DontDestroy

--- Assets/Scripts/MainScene.cs
+++ Assets/Scripts/MainScene.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 
 public class MainScene : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-    
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void Awake() {
+
     }
 
     public void ChangeOptionScene()
