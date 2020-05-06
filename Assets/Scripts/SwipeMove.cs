7ec12d189744f787fb2e806c6dea57eeac64f7f1
leivoev <leivoev@gmail.com>
2020-05-06 21:33:49 +0900

    Add Player Character
    But it doesn't move yet.

---
+++ Assets/Scripts/SwipeMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwipeMove : MonoBehaviour
+{
+    public Swipe swipeControls;
+    public Transform player;
+    private Vector3 desiredPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (swipeControls.SwipeLeft)
+          desiredPosition += Vector3.left;
+        if (swipeControls.SwipeRight)
+          desiredPosition += Vector3.right;
+        if (swipeControls.SwipeUp)
+          desiredPosition += Vector3.forward;
+        if (swipeControls.SwipeDown)
+          desiredPosition += Vector3.back;
+
+        player.transform.position = Vector3.MoveTowards(player.transform.position, desiredPosition, 3f * Time.deltaTime);
+        if (swipeControls.Tap)
+            Debug.Log("Tap");
+    }
+}
