e719c7b778e63cf646be20805c60f5404d6eaf4d
JangChorong <sweatpotato13@gmail.com>
2020-05-10 17:38:19 +0900

    Make Collision to emeny

--- Assets/Scripts/MoveTest.cs
+++ Assets/Scripts/MoveTest.cs
@@ -1,23 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 public class MoveTest : MonoBehaviour
 {
     // Start is called before the first frame update
     bool touch = false;
-    void Start()
-    {
-    }
     private Vector3 startPos = Vector3.zero;
   	private Vector3 endPos = Vector3.zero;
   	private Vector3 targetPos = Vector3.zero;
     private Vector3 mousepos = Vector3.zero;
   	private bool isClicked = false;
+
+    void Start()
+    {
+
+    }
+
     // Update is called once per frame
   	void Update()
   	{
   		DragObject();
   	}
+
   	void DragObject()
   	{
   		if (Input.GetMouseButtonDown(0))
@@ -32,17 +37,25 @@ public class MoveTest : MonoBehaviour
   		}
   		else if (isClicked)
   		{
-        //Debug.Log("isClicked");
-        Debug.Log("Mouse: " + Input.mousePosition);
-        Debug.Log("Player: " + transform.position);
+        	//Debug.Log("isClicked");
+        	//Debug.Log("Mouse: " + Input.mousePosition);
   			if (Input.GetMouseButton(0))
-        mousepos = Input.mousePosition;
-        mousepos.z = 10000;
+        	mousepos = Input.mousePosition;
+        	mousepos.z = 10000;
   			endPos = Camera.main.ScreenToWorldPoint(mousepos);
-        Debug.Log("Mouse(World): " + endPos);
+        	//Debug.Log("Mouse(World): " + endPos);
   			targetPos = endPos - startPos;
-        targetPos = new Vector3(targetPos.x, targetPos.y, gameObject.transform.position.z);
-        transform.position = targetPos;
+        	targetPos = new Vector3(targetPos.x, targetPos.y, gameObject.transform.position.z);
+        	transform.position = targetPos;
+		    //Debug.Log("Player: " + transform.position);
   		}
   	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+	    if (coll.gameObject.tag == "Bullet")
+	    {
+			Destroy(this.gameObject);
+			SceneManager.LoadScene("StageSelectScene");
+	    }
+	}
 }
