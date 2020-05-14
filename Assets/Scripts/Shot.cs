f6f3bfc52adbebb118942bbe1163545e6a8f22e3
JangChorong <sweatpotato13@gmail.com>
2020-05-14 21:12:35 +0900

    Boss makes bullet and makes player die

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    public int damage = 1;
+	public bool isEnemyShot = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy (gameObject, 20); //20초 삭제
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
