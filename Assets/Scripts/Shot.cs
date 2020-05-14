3806b1bdc6b3a7eb84e4c32db7fb6a5773eac59c
JangChorong <sweatpotato13@gmail.com>
2020-05-14 21:32:09 +0900

    Make bullet destroy when located out of screen

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -6,15 +6,19 @@ public class Shot : MonoBehaviour
 {
     public int damage = 1;
 	public bool isEnemyShot = false;
+    Vector3 pos;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy (gameObject, 20); //20초 삭제
+        //Destroy (gameObject, 20); //20초 삭제
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pos = Camera.main.WorldToScreenPoint(transform.position);
+        if(pos.y < -50){
+            Destroy(gameObject);
+        }
     }
 }
