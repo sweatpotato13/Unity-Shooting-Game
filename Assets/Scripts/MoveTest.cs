8f6c9c50fa698d7cb2813da20c4872faaf6209b3
leivoev <leivoev@gmail.com>
2020-05-16 00:12:15 +0900

    Player Moves

--- Assets/Scripts/MoveTest.cs
+++ Assets/Scripts/MoveTest.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class MoveTest : MonoBehaviour
 {
     // Start is called before the first frame update
-    bool touch = false;
     void Start()
     {
     }
@@ -13,12 +12,32 @@ public class MoveTest : MonoBehaviour
   	private Vector3 targetPos = Vector3.zero;
     private Vector3 mousepos = Vector3.zero;
   	private bool isClicked = false;
-
+    public float speed = 0.1f;
     // Update is called once per frame
   	void Update()
   	{
-  		  DragObject();
+  		  //DragObject();
+        SlideMove();
   	}
+
+    void SlideMove()
+    {
+        if (Input.touchCount >0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            Debug.Log("Touch: " + touchDeltaPosition);
+            Debug.Log("Player: " + transform.position);
+            transform.Translate(touchDeltaPosition.x * speed, touchDeltaPosition.y * speed, 0);
+        }
+        Vector3 worldpos = Camera.main.WorldToViewportPoint(this.transform.position);
+        if (worldpos.x < 0f) worldpos.x = 0f;
+        if (worldpos.y < 0f) worldpos.y = 0f;
+        if (worldpos.x > 1f) worldpos.x = 1f;
+        if (worldpos.y > 1f) worldpos.y = 1f;
+        this.transform.position = Camera.main.ViewportToWorldPoint(worldpos);
+    }
+
+
   	void DragObject()
   	{
   		if (Input.GetMouseButtonDown(0))
@@ -45,6 +64,7 @@ public class MoveTest : MonoBehaviour
         targetPos = new Vector3(targetPos.x, targetPos.y, gameObject.transform.position.z);
         transform.position = targetPos;
   		}
+
   	}
 
 }
