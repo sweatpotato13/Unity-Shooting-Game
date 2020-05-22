6c1b6f80c5889ceab669f06f4422062ecb2a24bd
leivoev <leivoev@gmail.com>
2020-05-22 18:05:39 +0900

    Add RandomAttack Pattern
    Pattern has limit degree.

--- Assets/Scripts/BossWeapon.cs
+++ Assets/Scripts/BossWeapon.cs
@@ -6,7 +6,7 @@ public class BossWeapon : MonoBehaviour
 {
     public Transform shotPrefab;
 	public float shootingRate = 0.25f;
-
+	public float rotationLimitDegree = 30.0f;
 	private float shootCooldown;
 
 	void Start () {
@@ -44,4 +44,35 @@ public class BossWeapon : MonoBehaviour
 
 		}
 	}
+	public void RandomAttack(bool isEnemy){
+		if (CanAttack) {
+			shootCooldown = shootingRate;
+
+			Transform shotTransform = Instantiate (shotPrefab) as Transform;
+			shotTransform.position = transform.position;
+
+			Shot tempShot = shotTransform.gameObject.GetComponent<Shot> ();
+			if (tempShot != null) {
+				tempShot.isEnemyShot = isEnemy;
+			}
+
+			Move tempMove = shotTransform.gameObject.GetComponent<Move> ();
+			if (tempMove != null) {
+				tempMove.direction = new Vector2(0,-1);//transform.up;
+				float rotationDegree = Random.Range(-(rotationLimitDegree),rotationLimitDegree);
+				tempMove.direction = Rotate(tempMove.direction, rotationDegree);
+			}
+		}
+	}
+
+    public Vector2 Rotate(Vector2 v, float degrees) {
+        float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+        float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+         
+        float tx = v.x;
+        float ty = v.y;
+        v.x = (cos * tx) - (sin * ty);
+        v.y = (sin * tx) + (cos * ty);
+        return v;
+    }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,7 +13,7 @@ public class Enemy : MonoBehaviour
 	void Update () {
 		for (int i = 0; i < myWeapons.Length; i++) {
 			if (myWeapons[i] != null && myWeapons[i].CanAttack) {
-				myWeapons[i].Attack (true);
+				myWeapons[i].RandomAttack (true);
 			}	
 		}
 
