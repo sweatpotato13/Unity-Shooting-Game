6e6ea9b220da39276f13afee9ef25b816e31659f
JangChorong <sweatpotato13@gmail.com>
2020-05-24 12:41:39 +0900

    Make Boss HPbar

---
+++ Assets/Scripts/HPbar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPbar : MonoBehaviour
+{
+    public Slider mainSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainSlider.value = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject temp = GameObject.Find("Enemy");
+        float val;
+        if(temp != null)
+            val = temp.GetComponent<Health>().getHprate();
+        else
+            val = 0;
+        Debug.Log(val);
+        mainSlider.value = val;
+    }
+}
