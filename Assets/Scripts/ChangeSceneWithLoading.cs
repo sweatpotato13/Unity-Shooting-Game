52af7fffabbf19dc4f6d91266b220e8a6dca121a
leivoev <leivoev@gmail.com>
2020-06-13 02:54:43 +0900

    Add Result Scene
    If Enemy die, Result Panel will be active state

---
+++ Assets/Scripts/ChangeSceneWithLoading.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneWithLoading : MonoBehaviour
+{
+     public void ChangeScene(string sceneName)
+    {
+        LoadingSceneManager.LoadScene(sceneName);
+    }
+
+    public void ChangeInGameScene(int stageLevel)
+    {
+        
+        string sceneName = "InGameScene";
+        string level = stageLevel.ToString();
+        LoadingSceneManager.LoadScene(sceneName + level);
+    }
+}
